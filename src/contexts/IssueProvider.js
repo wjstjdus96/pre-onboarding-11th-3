3e66c492e3b2423193994f813771a7dd5b220d6f
seoyeon <wjstjdus96@naver.com>
2023-07-13 02:15:35 +0900

    feat: IssueContext Provider 생성

---
+++ src/contexts/IssueProvider.js
@@ -0,0 +1,23 @@
+import { useContext, useState, useMemo } from "react";
+import { IssueActionContext, IssueValueContext } from "./IssueContext";
+
+export function IssueProvider({ children }) {
+  const [issues, setIssues] = useState();
+
+  const actions = useMemo(
+    () => ({
+      loadIssue(loadedIssue) {
+        setIssues((prev) => prev.concat(loadedIssue));
+      },
+    }),
+    []
+  );
+
+  return (
+    <IssueActionContext.Provider value={actions}>
+      <IssueValueContext.Provider value={issues}>
+        {children}
+      </IssueValueContext.Provider>
+    </IssueActionContext.Provider>
+  );
+}
